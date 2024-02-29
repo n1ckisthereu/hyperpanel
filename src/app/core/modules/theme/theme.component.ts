c3be736f3144f5d3c3cf0b7f926ee2ce9169c272
Andrew Domingues <drewdomi@proton.me>
2024-02-28 23:59:16 -0300

    feat: add colorscheme for applyed wallpaper

--- src/app/core/modules/theme/theme.component.ts
+++ src/app/core/modules/theme/theme.component.ts
@@ -3,11 +3,30 @@ import { Component } from "@angular/core";
 
 import { ImageModule } from "primeng/image";
 import { DividerModule } from "primeng/divider";
+import { ButtonModule } from "primeng/button";
+
+import { remixCheckFill } from "@ng-icons/remixicon";
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
 
 @Component({
   standalone: true,
   selector: "app-theme",
   templateUrl: "./theme.component.html",
-  imports: [CommonModule, ImageModule, DividerModule],
+  styleUrl: "./theme.component.scss",
+  viewProviders: [
+    provideIcons({
+      remixCheckFill,
+    }),
+  ],
+  imports: [
+    CommonModule,
+    NgIconComponent,
+    ImageModule,
+    DividerModule,
+    ButtonModule,
+  ],
 })
-export class ThemeComponent {}
+export class ThemeComponent {
+  currentWallpaper =
+    "https://primefaces.org/cdn/primeng/images/galleria/galleria1.jpg";
+}
